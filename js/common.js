bf9fdca7aa42c4e62f3ffc1fe4ba4ee0d7a0501c
547011926 <547011926@qq.com>
2017-12-14 09:22:52 +0800

    common.js封装

--- js/common.js
+++ js/common.js
@@ -7,22 +7,22 @@
  @param callback function
  */
 
-function doAjax(type, url, isAsync, data, callback){
+function doAjax(type, url, isAsync, data, callback) {
     //创建通信对象
     var xhr = createXhr();
 
     //判断通信类型
-    if(type.toLowerCase() === "get"){
+    if (type.toLowerCase() === "get") {
         //遍历拼接数据到url
-        url +="?";
-        for(var n in data){
-            url += n+ "=" + data[n]+"&";
+        url += "?";
+        for (var n in data) {
+            url += n + "=" + data[n] + "&";
         }
         //截掉最后一位
-        url = url.substr(0, url.length-1)
+        url = url.substr(0, url.length - 1)
         //释放data
         data = null;
-    }else{
+    } else {
         data = JSON.stringify(data);
     }
 
@@ -32,9 +32,9 @@ function doAjax(type, url, isAsync, data, callback){
     xhr.send(data);
 
     //监听readyState变化
-    xhr.onreadystatechange = function(){
-        if((xhr.status>=200 && xhr.status<300) || xhr.status == 304){
-            if(xhr.readyState === 4){
+    xhr.onreadystatechange = function () {
+        if ((xhr.status >= 200 && xhr.status < 300) || xhr.status == 304) {
+            if (xhr.readyState === 4) {
                 var data = JSON.parse(xhr.responseText);
                 //传递给回调函数
                 callback && callback(data);
@@ -44,71 +44,71 @@ function doAjax(type, url, isAsync, data, callback){
 }
 
 //创建通信对象
-function createXhr(){
+function createXhr() {
     //大部分浏览器中使用XMLHttpRequest创建通信对象
-    if(typeof XMLHttpRequest !== "undefined"){
+    if (typeof XMLHttpRequest !== "undefined") {
         return new XMLHttpRequest();
-    }else{
+    } else {
         //IE6及ie6以下浏览器不支持XMLHttpRequest 这些浏览器使用ActiveXObject创建通信对象，
         //在创建对象是需要根据浏览器具体版本传入不同的参数 如下： "MSXML.XMLHttp.6.0","MSXML.XMLHttp.3.0","MSXML.XMLHttp", "Microsoft.XMLHTTP"
-        var args = ["MSXML.XMLHttp.6.0","MSXML.XMLHttp.3.0","MSXML.XMLHttp", "Microsoft.XMLHTTP"];
-        for(var n=0; n<args.length; n++){
+        var args = ["MSXML.XMLHttp.6.0", "MSXML.XMLHttp.3.0", "MSXML.XMLHttp", "Microsoft.XMLHTTP"];
+        for (var n = 0; n < args.length; n++) {
             //异常处理
-            try{
+            try {
                 var xhr = new ActiveXObject(args[n]);
                 return xhr;
-            }catch(e){
+            } catch (e) {
                 console.log(e);
             }
         }
     }
 }
 
+//增删改查页面的逻辑
 //点击增加按钮时
-function add(){
+function add() {
     $(".alertbox").show();
     $(".add").show();
     $(".del").hide();
     $(".find").hide();
 
     //点击取消关闭弹窗
-    $(".add .btn2").click(function(){
+    $(".add .btn2").click(function () {
         $(".add").hide();
         $(".alertbox").hide();
     })
 }
 
 //点击删除按钮时
-function del(e){
+function del(e) {
     $(".alertbox").show();
     $(".add").hide();
     $(".find").hide();
     $(".revise").hide();
     $(".del").show();
     //console.log($(e.target).closest("tr"));
-    $(".del>.btn1").click(function(){
+    $(".del>.btn1").click(function () {
         $(e.target).closest("tr").remove();
         $(".alertbox").hide();
     })
-    $(".del>.btn2").click(function(){
+    $(".del>.btn2").click(function () {
         $(".alertbox").hide();
         $(".del").hide();
     })
-};
+}
 
 //点击修改按钮时
-function revise(){
+function revise() {
     console.log("我是修改函数")
-}
+};
 
 //点击查看按钮时
-function find(){
+function find() {
     console.log("我是查找函数")
-}
+};
 
-//正则验证
-function verify(){
-    //昵称
+//正则昵称验证
+function verify() {
     var h = /^[\u4E00-\u9FA5A-Za-z0-9_]+$/;
 
     $("[name='name']").blur(function () {
@@ -125,8 +125,8 @@ function verify(){
         }
     });
 }
-function userName(){
-    //手机号
+//正则手机号验证
+function userName() {
     var d = /^1[3|4|5|7|8][0-9]\d{4,8}$/;
     $("[name='userName']").blur(function () {
         var v = $(this).val();
@@ -142,10 +142,8 @@ function userName(){
         }
     });
 }
-
-
-function password(){
-    //密码
+//正则密码验证
+function password() {
     var p = /^[a-zA-Z]\w{5,17}$/;
     $("[name='password']").blur(function () {
         var v = $(this).val();
@@ -161,9 +159,8 @@ function password(){
         }
     });
 }
+//正则邮箱验证
 function email() {
-
-//邮箱
     var y = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+/;
     $("[name='email']").blur(function () {
         var v = $(this).val();
@@ -179,9 +176,8 @@ function email() {
         }
     });
 }
+//正则身份证验证
 function idCard() {
-
-//身份证
     var idCard = /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/;
     $("[name='idCard']").blur(function () {
         var v = $(this).val();
@@ -196,24 +192,32 @@ function idCard() {
             $("[name='idCard']").next().html("正确");
         }
     });
-
 }
-/*
-$("input").focus(function () {
-    $(this).prev().css("color", "#008DE8");
-});
-*/
 
 
-
-
-
-
-}
 
 //用户状态启停页面的逻辑
 //	按钮开关
+function anniu(obj){
+    if ($(obj).find("span").css("left") == "2px") {
+        $(obj).addClass("active");
+    } else {
+        $(obj).removeClass("active");
+    }
+}
+//删除选中的行
+function del_anniu(obj){
+    var c=$(obj).closest("tr")[0];
+//    console.log(c)
+    var checkbox=$(obj).closest("tr").find("input[type='checkbox']")[0];
+//判断checkbox的状态是不是选中
+    if($(checkbox).is(':checked')) {
+//        alert(11)
+        c.remove()
+    }else{
+        alert('删除选中的信息')
+    }
 
-
+}
 
 
