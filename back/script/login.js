268a71fc670a925d04e1de9da7fc04b763634f09
kolby <kolby.yu@e-innotech.com>
2018-01-02 15:53:18 +0800

    login finish

--- back/script/login.js
+++ back/script/login.js
@@ -2,7 +2,7 @@ $(function () {
 
     config.init();
     $("#login").click(function (){
-        var data = {'userName':$('.username').val(),'passWord':$('.password').val()};
+        var user = {'userName':$('.username').val(),'passWord':$('.password').val()};
 
 
         $.ajax({
@@ -10,10 +10,19 @@ $(function () {
             url:$admin.login,
             contentType:'application/json',
             dataType:'json',
-            data:JSON.stringify(data),
+            data:JSON.stringify(user),
             success:function (data) {
                 console.log(data);
                 //判断如果登录成功，就跳转到home页面，并且存储权限数据到sessionstorage
+                if(data.success){
+                    if($('.remember').attr('checked')){
+                        sessionStorage.setItem('user',user);
+                    }
+                    sessionStorage.setItem('permissons',data.data);
+                    location.replace('home.html');
+                }else{
+                    alert(data.msg);
+                }
             }
         });
     });
