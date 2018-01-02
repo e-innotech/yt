3d95db4a2896c505ab1eeaa5b3751ecef0d17b7b
kolby <kolby.yu@e-innotech.com>
2018-01-02 15:35:27 +0800

    login

--- back/script/login.js
+++ back/script/login.js
@@ -13,7 +13,7 @@ $(function () {
             data:JSON.stringify(data),
             success:function (data) {
                 console.log(data);
-
+                //判断如果登录成功，就跳转到home页面，并且存储权限数据到sessionstorage
             }
         });
     });
