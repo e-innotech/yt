67195a05a2cdd51f4910e5f6b9b2f4b9d8b27534
547011926 <547011926@qq.com>
2018-01-03 17:28:27 +0800

    修改密码 登出

--- back/script/home.js
+++ back/script/home.js
@@ -25,7 +25,6 @@ $(function(){
                             if(data.success){
                                 $('#pwdResetModal').hide();
                             }
-
                         }
                     });
                 });
