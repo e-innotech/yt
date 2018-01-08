824a07a4898620dda748e306d268b108433899b9
kolby <kolby.yu@e-innotech.com>
2018-01-08 11:02:44 +0800

    news

--- common/js/config.js
+++ common/js/config.js
@@ -1,6 +1,7 @@
 var config ={
     init:function(){
-        $apiUrl = ' http://192.168.20.195:8080';
+        // $apiUrl = ' http://192.168.20.195:8080';
+        $apiUrl = ' http://123.59.156.27:8080';
         $admin = {
             'login':$apiUrl+'/login',
             'logout':$apiUrl+'/logout'
