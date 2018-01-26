e3cf76c1a814a2c7d338fa6190a2f9fdb574ab9a
fanlihua <1553830668@qq.com>
2018-01-26 18:55:10 +0800

    顶端财经

--- front/topFinancial/script/js/top.js
+++ front/topFinancial/script/js/top.js
@@ -9,9 +9,8 @@ $(function() {
         window.sessionStorage.setItem("global_search_data", JSON.stringify(data));
         location.href = 'search.html';
     };
-//搜索
+//点击搜索按钮时
     $("#search").click(function () {
-        alert(11111);
         var newsTitle = $('input[name="search"]').val();
         console.log(7, newsTitle);
         if (newsTitle == '') {
@@ -21,4 +20,28 @@ $(function() {
         //参数 网站id 新闻标题 当前页 显示页
         globalQuery(3, newsTitle, 1, 5, getSearchList);
     });
+
+
+    //添加样式
+    //$(".nav a").each(function(){
+    //    $this = $(this);
+    //    if($this[0].href==String(window.location)){
+    //       $this.addClass("active");  //hover表示被选中效果的类名
+    //
+    //    }
+    //})
+
+//
+//function styles($this){
+//    $($this).css("color","#f8fbfa");
+//    $($this.parent()).css("background","#e73929");
+//    console.log(3333);
+//}
+
+
+
+
+
+
+
 })
