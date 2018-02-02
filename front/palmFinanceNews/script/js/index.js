71e15cdf98a0730da1668a2a2fbdb70f00a46204
547011926 <547011926@qq.com>
2018-02-02 10:24:23 +0800

    掌上财经

---
+++ front/palmFinanceNews/script/js/index.js
@@ -0,0 +1,50 @@
+
+$(document).ready(function(){
+
+    $('.back_to_top').Totop({
+        autohide:true,//设置元素是否一开始就隐藏，
+        speed:500,//到顶部速度
+        right:274,
+        bottom:210,
+        offsetTop:400,//当scrollTop等于这个值时，如果autohide:true元素显示
+        position:true//是否采用fixed定位
+    })
+
+    // 加载更多
+    var commentData='';
+    var page=1;
+    function getHomeNewsList() {
+        //传的权限 页数 一页显示几条数据
+        homeList(3, page,1, function callback(newsList) {
+            console.log(newsList);
+            commentData = newsList;
+            for (var i = 0; i < newsList.length; i++) {
+                $('#moreList').append('<li>' +
+                '<a href="">' +
+                '<div>' +
+                '<div class="img_box">' +
+                '<img src=' + newsList[i].topImagePath + ' alt="" onload="drawImage(this,240,170)"/>' +
+                '</div>' +
+                '<h3>' +newsList[i].newsTitle+'</h3>' +
+                '<p>' +newsList[i].subContent+'</p>' +
+                '<div class="list_info">' +
+                '<span class="source"><a href="">' +newsList[i].source+'</a></span>' +
+                '<span class="time">' +newsList[i].createDate+'</span>' +
+                '</div>' +
+                '</div>' +
+                '</a>' +
+                '</li>')
+            }
+        })
+    };
+
+    /*分页*/
+    $('.more').click(function(){
+        if (commentData.length < 0) {
+            $('.more').html('没有更多了');
+        } else {
+            page++;
+            getHomeNewsList();
+        }
+    })
+});
